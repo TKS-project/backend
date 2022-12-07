e9c87eb586f220d62bcb771d6959b738083a21f9
Kantaro0829 <eshangwan@gmail.com>
2022-12-07 11:20:55 +0900

    [update] 日付時間を指定して公共交通機関での乗り換えルートを取得する

--- app/usecase/scrape_repository.go
+++ app/usecase/scrape_repository.go
@@ -1,5 +1,19 @@
 package usecase
 
+import "github.com/Kantaro0829/clean-architecture-in-go/domain"
+
 type ScrapeRepository interface {
 	FindMeaning(word string) string
+
+	FindTransports() string
+
+	FindOneTransport(
+		orvStation string,
+		dnvStation string,
+		year string,
+		month string,
+		day string,
+		hour string,
+		minute string,
+	) domain.TransportsJson
 }
